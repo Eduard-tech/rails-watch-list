c208b90c6e06b42e853fc4ac0f3d65bb10baa636
Eduard Ciugulea <ciugulea@yahoo.com>
2021-05-26 22:09:29 +0100

    seed api

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,4 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list_id, message: "Wrong" }
-  validates :list_id, uniqueness: { scope: :movie_id, message: "Wrong" }
 end
